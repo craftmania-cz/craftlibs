8bd34346f7d6e8f78a6411baa9ccfefb84dd231f
Tomáš Fiala <tomasfiala555@gmail.com>
2019-12-30 20:50:57 +0100

    Async query metoda

--- src/main/java/cz/craftmania/craftlibs/sql/SQLManager.java
+++ src/main/java/cz/craftmania/craftlibs/sql/SQLManager.java
@@ -2,10 +2,14 @@ package cz.craftmania.craftlibs.sql;
 
 import cz.craftmania.craftlibs.CraftLibs;
 import cz.craftmania.craftlibs.utils.Log;
+import org.bukkit.Bukkit;
 
 import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.function.Supplier;
 
 public class SQLManager {
 
@@ -27,67 +31,69 @@ public class SQLManager {
         return pool;
     }
 
-    public ArrayList<DBRow> query(String query, Object... variables) {
-        final Long time = System.currentTimeMillis();
-        final ArrayList<DBRow> rows = new ArrayList<>();
-        ResultSet result = null;
-        PreparedStatement pState = null;
-        try {
-            pState = this.pool.getConnection().prepareStatement(query);
-            for (int i = 1; i <= variables.length; ++i) {
-                Object obj = variables[i - 1];
-                if (obj != null && obj.toString().equalsIgnoreCase("null")) {
-                    obj = null;
-                }
-                if (obj instanceof Blob) {
-                    pState.setBlob(i, (Blob) obj);
-                } else if (obj instanceof InputStream) {
-                    pState.setBinaryStream(i, (InputStream) obj);
-                } else if (obj instanceof byte[]) {
-                    pState.setBytes(i, (byte[]) obj);
-                } else if (obj instanceof Boolean) {
-                    pState.setBoolean(i, (boolean) obj);
-                } else if (obj instanceof Integer) {
-                    pState.setInt(i, (int) obj);
-                } else if (obj instanceof String) {
-                    pState.setString(i, (String) obj);
-                } else {
-                    pState.setObject(i, obj);
-                }
-            }
-            if (pState.execute()) {
-                result = pState.getResultSet();
-            }
-            if (result != null) {
-                final ResultSetMetaData mtd = result.getMetaData();
-                final int columnCount = mtd.getColumnCount();
-                while (result.next()) {
-                    final DBRow row = new DBRow();
-                    for (int l = 0; l < columnCount; ++l) {
-                        final String columnName = mtd.getColumnName(l + 1);
-                        row.addCell(columnName, result.getObject(columnName));
+    public CompletableFuture<ArrayList<DBRow>> query(String query, Object... variables) {
+        CompletableFuture<ArrayList<DBRow>> completableFuture = new CompletableFuture<>();
+
+        Bukkit.getScheduler().runTaskAsynchronously(CraftLibs.getInstance(), () -> {
+            final Long time = System.currentTimeMillis();
+            final ArrayList<DBRow> rows = new ArrayList<>();
+            Connection connection = null;
+            ResultSet result = null;
+            PreparedStatement pState = null;
+            try {
+                connection = pool.getConnection();
+                pState = connection.prepareStatement(query);
+                for (int i = 1; i <= variables.length; ++i) {
+                    Object obj = variables[i - 1];
+                    if (obj != null && obj.toString().equalsIgnoreCase("null")) {
+                        obj = null;
                     }
-                    rows.add(row);
+                    if (obj instanceof Blob) {
+                        pState.setBlob(i, (Blob) obj);
+                    } else if (obj instanceof InputStream) {
+                        pState.setBinaryStream(i, (InputStream) obj);
+                    } else if (obj instanceof byte[]) {
+                        pState.setBytes(i, (byte[]) obj);
+                    } else if (obj instanceof Boolean) {
+                        pState.setBoolean(i, (boolean) obj);
+                    } else if (obj instanceof Integer) {
+                        pState.setInt(i, (int) obj);
+                    } else if (obj instanceof String) {
+                        pState.setString(i, (String) obj);
+                    } else {
+                        pState.setObject(i, obj);
+                    }
                 }
-            }
-        } catch (Exception ex) {
-            Log.fatal("Error has occured in query: '" + query + "'");
-            ex.printStackTrace();
-        } finally {
-            try {
-                result.close();
+                if (pState.execute()) {
+                    result = pState.getResultSet();
+                }
+                if (result != null) {
+                    final ResultSetMetaData mtd = result.getMetaData();
+                    final int columnCount = mtd.getColumnCount();
+                    while (result.next()) {
+                        final DBRow row = new DBRow();
+                        for (int l = 0; l < columnCount; ++l) {
+                            final String columnName = mtd.getColumnName(l + 1);
+                            row.addCell(columnName, result.getObject(columnName));
+                        }
+                        rows.add(row);
+                    }
+                    completableFuture.complete(rows);
+                }
             } catch (Exception ex) {
+                Log.fatal("Error has occured in query: '" + query + "'");
+                completableFuture.completeExceptionally(new Exception("Error has occured in query: '" + query + "'"));
+                ex.printStackTrace();
+            } finally {
+                pool.close(connection, pState, result);
             }
-            try {
-                pState.close();
-            } catch (Exception ex2) {
+            final Long diff = System.currentTimeMillis() - time;
+            if (diff > 500L) {
+                Log.fatal("This query is taking too long (" + diff + "ms): '" + query + "'");
             }
-        }
-        final Long diff = System.currentTimeMillis() - time;
-        if (diff > 500L) {
-            Log.fatal("This query is taking too long (" + diff + "ms): '" + query + "'");
-        }
-        return rows;
+        });
+
+        return completableFuture;
     }
 
 }
