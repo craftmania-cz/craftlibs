4e90d75ebc1163e4a8f229af8aa61dc63b60bbf3
MrWakeCZ <joseff.kral@gmail.com>
2021-03-05 19:13:43 +0100

    ChatInfo: Economy zpráva extra

--- src/main/java/cz/craftmania/craftlibs/utils/ChatInfo.java
+++ src/main/java/cz/craftmania/craftlibs/utils/ChatInfo.java
@@ -10,7 +10,8 @@ public enum ChatInfo {
 
     INFO("⻫", new Color(244, 158, 11)),
     DANGER("⻬", new Color(220, 38, 37)),
-    SUCCESS("⻪", new Color(133, 204, 22));
+    SUCCESS("⻪", new Color(133, 204, 22)),
+    ECONOMY("⻮", new Color(17, 96, 67));
 
     private final String key;
     private final Color color;
@@ -20,11 +21,11 @@ public enum ChatInfo {
         this.color = color;
     }
 
-    public void sendTo(Player player, String message) {
+    public void send(Player player, String message) {
         player.sendMessage(this.key + " " + ChatColor.of(this.color) + message);
     }
 
-    public void sendTo(String player, String message) {
+    public void send(String player, String message) {
         if (Bukkit.getPlayer(player) == null) {
             return;
         }
