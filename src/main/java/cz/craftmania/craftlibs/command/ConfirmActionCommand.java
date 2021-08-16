e1f848e2fff798052e6b737fc640a5f6e5772cd1
MrWakeCZ <joseff.kral@gmail.com>
2021-08-16 03:43:13 +0200

    Update ChatInfo & doplnění JavaDocs

--- src/main/java/cz/craftmania/craftlibs/command/ConfirmActionCommand.java
+++ src/main/java/cz/craftmania/craftlibs/command/ConfirmActionCommand.java
@@ -5,13 +5,14 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
 public class ConfirmActionCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (!(sender instanceof Player)) return true;
         Player player = (Player) sender;
 
--- src/main/java/cz/craftmania/craftlibs/utils/ChatInfo.java
+++ src/main/java/cz/craftmania/craftlibs/utils/ChatInfo.java
@@ -3,35 +3,96 @@ package cz.craftmania.craftlibs.utils;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
 
+/**
+ * Metoda k zasílání jednotných zpráv na serveru dle typu (klíče) a prefixu z resource packu.
+ */
 public enum ChatInfo {
 
+    /**
+     * Základní informační zpráva pro jakýkoliv typ zprávy.
+     */
     INFO("⻫", new Color(243, 196, 76)),
+
+    /**
+     * Vystražná zpráva, s vyšší prioritou upozornění.
+     */
     DANGER("⻬", new Color(239, 87, 86)),
+
+    /**
+     * Zpráva o úspěšné provedené akci.
+     */
     SUCCESS("⻪", new Color(84, 160, 63)),
+
+    /**
+     * Zpráva používající se hlavně k zasílání informačních ekonomických zpráv.
+     */
     ECONOMY("⻮", new Color(35, 120, 90)),
-    SERVER("", new Color(88, 130, 180));
+
+    /**
+     * Zpráva s vyšší prioritou a prefixem [Server].
+     */
+    SERVER("⼧", new Color(88, 130, 180));
 
     private final String key;
     private final Color color;
+    private String message;
 
     ChatInfo(String key, Color color) {
         this.key = key;
         this.color = color;
+        this.message = "";
     }
 
-    public void send(Player player, String message) {
-        player.sendMessage(this.key + " " + ChatColor.of(this.color) + message);
+    /**
+     * Odešle zvolenou zprávu online hráči.
+     *
+     * Barvu dle klíče {@link ChatInfo} lze v textu používat jako "{c}"
+     *
+     * @param player Online hráč
+     * @param message Zpráva, co se pošle hráči
+     * @return {@link ChatInfo}
+     */
+    public ChatInfo send(@NotNull Player player, @NotNull String message) {
+        this.message = message;
+        player.sendMessage(this.key + " " + ChatColor.of(this.color) + message.replace("{c}",  ChatColor.of(this.color).toString()));
+        return this;
     }
 
-    public void send(String player, String message) {
+    /**
+     * Odešle vytvořenou zprávu hráči, pokud je online.
+     * Pokud hráč není online, metoda neprovede nic a vrátí void.
+     *
+     * Barvu dle klíče {@link ChatInfo} lze v textu používat jako "{c}"
+     *
+     * @param player Online hráč
+     * @param message Zpráva, co se pošle hráči
+     * @return {@link ChatInfo}
+     */
+    @Nullable
+    public ChatInfo send(@NotNull String player, @NotNull String message) {
+        this.message = message;
         if (Bukkit.getPlayer(player) == null) {
-            return;
+            return null;
         }
         Player onlinePlayer = Bukkit.getPlayer(player);
         assert onlinePlayer != null;
-        onlinePlayer.sendMessage(this.key + " " + ChatColor.of(this.color) + message);
+        onlinePlayer.sendMessage(this.key + " " + ChatColor.of(this.color) + message.replace("{c}",  ChatColor.of(this.color).toString()));
+        return this;
+    }
+
+    /**
+     * Vrací zadanou zprávu při vytváření objektu {@link ChatInfo}<br>
+     * 
+     * <strong>Zpráva je v raw formátu bez barev podle klíče objektu.</strong>
+     *
+     * @return String zprávy
+     */
+    public @NotNull String getMessage() {
+        return message;
     }
 }
